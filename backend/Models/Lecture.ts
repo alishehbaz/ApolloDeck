b1497faef2a08ba164ac2f9586fcd83b0ad18528
Ali Shehbaz <alishahbaz072@gmail.com>
2023-04-02 03:24:45 -0400

    add: routes for lectures

--- backend/Models/Lecture.ts
+++ backend/Models/Lecture.ts
@@ -1,21 +1,23 @@
-import { Schema, model, connect } from 'mongoose';
+import { Schema, model, connect } from "mongoose";
 
 // 1. Create an interface representing a document in MongoDB.
 export interface ILecture {
-    path: string;
-    course: string;
-    tone?: string;
-    reminder?: boolean;
-    mintAsNFT?: boolean;
-    IPFS?:boolean;
-  }
-  
+  lectureId: string;
+  title: string;
+  courseId: string;
+  course: string;
+  status: string;
+  hasNFT: boolean;
+  slidesURL: string;
+}
+
 // 2. Create a Schema corresponding to the document interface.
 export const lectureSchema = new Schema<ILecture>({
-    path: { type: String, required: true },
-    course: { type: String, required: true },
-    tone: String,
-    reminder: Boolean,
-    mintAsNFT: Boolean,
-    IPFS: Boolean,
-  });
+  lectureId: { type: String, required: true },
+  title: { type: String, required: true },
+  courseId: { type: String, required: true },
+  course: { type: String, required: true },
+  status: { type: String, required: true },
+  hasNFT: { type: Boolean, required: true },
+  slidesURL: { type: String, required: true },
+});
