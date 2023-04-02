e7793426233b020bcacd4d7542e39114691397b3
Zixuan James Li <p359101898@gmail.com>
2023-04-02 05:17:22 -0400

    add: finish up LectureDetails.
    Signed-off-by: Zixuan James Li <p359101898@gmail.com>

--- frontend/src/pages/LectureDetails.tsx
+++ frontend/src/pages/LectureDetails.tsx
@@ -2,9 +2,22 @@ import * as React from "react";
 import { LectureProp } from "../components/SquareBox";
 import Centered from "../layouts/Centered";
 import { getLecture } from "../services/lectures";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Loading from "../components/Loading";
-import { Box, Heading, Card, CardHeader, CardBody, CardFooter, Text, Stack, StackDivider , Button, Divider, ButtonGroup} from '@chakra-ui/react';
+import {
+  Box,
+  Heading,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Text,
+  Stack,
+  StackDivider,
+  Button,
+  Divider,
+  ButtonGroup,
+} from "@chakra-ui/react";
 
 const LectureDetails = () => {
   const params = useParams();
@@ -28,62 +41,66 @@ const LectureDetails = () => {
           : `@ ${lecture.course} / ${lecture.title}`
       }
     >
-    {lecture === undefined ? (
-      <Loading />
-    ) : (
-<Card>
-  <CardHeader>
-    <Heading size='md'>Lecture details </Heading>
-  </CardHeader>
+      {lecture === undefined ? (
+        <Loading />
+      ) : (
+        <Card>
+          <CardHeader>
+            <Heading size="md">Lecture details </Heading>
+          </CardHeader>
 
-  <CardBody>
-    <Stack divider={<StackDivider />} spacing='4'>
-      <Box>
-        <Heading size='xs' textTransform='uppercase'>
-          Title
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          [Title title title title title].
-        </Text>
-      </Box>
-      <Box>
-        <Heading size='xs'  textTransform='uppercase'>
-         [NFT] Contract Address of Minting
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          [placeholder] 0xB4EDD991e2b6b7fa041122F5b9A82Eed2f740Be3
-        </Text>
-      </Box>
-      <Box>
-        <Heading size='xs' textTransform='uppercase' >
-         [NFT] Blockchain of contract address
-        </Heading>
-        <Text pt='2' fontSize='sm'>
-          [placeholder] mumbai
-        </Text>
-      </Box>
-    </Stack>
-  </CardBody>
-  <Divider />
-  <CardFooter>
-  <Stack>
-    <Box>
-  <Heading size='xs' textTransform='uppercase'>
-          Download
-        </Heading><Text></Text>
-        </Box>
-    <ButtonGroup spacing='2'>
-      <Button variant='solid' colorScheme='blue'>
-        IPFS (Web3)
-      </Button>
-      <Button variant='ghost' colorScheme='blue'>
-        .pptx
-      </Button>
-    </ButtonGroup></Stack>
-  </CardFooter>
-</Card>
-  )}
-    </Centered>    
+          <CardBody>
+            <Stack divider={<StackDivider />} spacing="4">
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  Title
+                </Heading>
+                <Text pt="2" fontSize="sm">
+                  {lecture.title}
+                </Text>
+              </Box>
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  Course
+                </Heading>
+                <Text pt="2" fontSize="sm">
+                  {lecture.course}
+                </Text>
+              </Box>
+            </Stack>
+          </CardBody>
+          <Divider />
+          <CardFooter>
+            <Stack>
+              <Box>
+                <Heading size="xs" textTransform="uppercase">
+                  View
+                </Heading>
+              </Box>
+              <ButtonGroup spacing="2">
+                <Link
+                  to={lecture.slidesURL ?? "https://slides.google.com"}
+                  target="_blank"
+                >
+                  <Button variant="solid" colorScheme="blue">
+                    Goolge Slides {lecture.slidesURL}
+                  </Button>
+                </Link>
+                <Button
+                  variant="disabled"
+                  colorScheme="blue"
+                  color="gray"
+                  _hover={{ cursor: "not-allowed" }}
+                  disabled
+                >
+                  Download .pptx
+                </Button>
+              </ButtonGroup>
+            </Stack>
+          </CardFooter>
+        </Card>
+      )}
+    </Centered>
   );
 };
 
